c07f8f98d6363c5a42d9e24e6bd043bdcacdfd6c
ViRust Hackfest <ivan.panska@gmail.com>
2023-10-03 22:21:22 +0700

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,272 @@
-import Image from 'next/image'
+'use client';
 
-export default function Home() {
+import React, { useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'; // Import CSS dari react-datepicker
+
+export default function Page() {
+  const [name, setName] = useState('');
+  const [nameArr, setNameArr] = useState([]);
+  const [birthDate, setBirthDate] = useState(new Date());
+  const [birthDateArr, setBirthDateArr] = useState([]);
+  const [recMedNumber, setRecMedNumber] = useState('');
+  const [recMedNumberArr, setRecMedNumberArr] = useState([]);
+  const [gender, setGender] = useState('laki-laki');
+  const [genderArr, setGenderArr] = useState([]);
+  const [num, setNum] = useState(0);
+  const [numArr, setNumArr] = useState([]);
+  const [isResultPage, setIsResultPage] = useState(false);
+  const [divElements, setDivElements] = useState([]);
+
+  const handleFormSubmit = (e) => {
+    const elements = [];
+
+    numArr.map((value, index) => {
+      for (let i = 0; i < value; i++) {
+        elements.push(
+          <div className='px-1 py-1'>
+            <div className="mycard" key={i}>
+              <table border="0">
+                {nameArr[index] !== '' ? (
+                  <tr>
+                    <td>Nama</td>
+                    <td>:</td>
+                    <td>{nameArr[index]}</td>
+                  </tr>
+                ) : (
+                  ''
+                )}
+                {birthDateArr[index] ? (
+                  <tr>
+                    <td>Tanggal Lahir</td>
+                    <td>:</td>
+                    <td>{birthDateArr[index]}</td>
+                  </tr>
+                ) : (
+                  ''
+                )}
+                {recMedNumberArr[index] ? (
+                  <tr>
+                    <td>No Rec.Med</td>
+                    <td>:</td>
+                    <td>{recMedNumberArr[index]}</td>
+                  </tr>
+                ) : (
+                  ''
+                )}
+                {genderArr[index] ? (
+                  <tr>
+                    <td>Jenis Kelamin</td>
+                    <td>:</td>
+                    <td>{genderArr[index]}</td>
+                  </tr>
+                ) : (
+                  ''
+                )}
+              </table>
+            </div>
+          </div>
+        );
+      }
+    });
+
+    setDivElements(elements);
+    setIsResultPage(true);
+    // Proses data yang di-submit di sini
+  };
+
+  const deleteData = (idx) => {
+    setNameArr((nameArr) => nameArr.filter((value, index) => index !== idx));
+    setBirthDateArr((birthDateArr) =>
+      birthDateArr.filter((value, index) => index !== idx)
+    );
+    setRecMedNumberArr((recMedNumberArr) =>
+      recMedNumberArr.filter((value, index) => index !== idx)
+    );
+    setGenderArr((genderArr) =>
+      genderArr.filter((value, index) => index !== idx)
+    );
+    setNumArr((numArr) => numArr.filter((value, index) => index !== idx));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      {!isResultPage && (
+        <>
+          <div className="max-w-md mx-auto p-4 font-black shadow-lg rounded-lg">
+            <h2 className="text-xl font-semibold mb-4">Form Data Pasien</h2>
+            <form>
+              <div className="mb-4">
+                <label htmlFor="name" className="block font-medium">
+                  Nama:
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="border border-gray-300 rounded-md w-full p-2 text-black font-light"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="birthDate" className="block font-medium">
+                  Tanggal Lahir:
+                </label>
+                <DatePicker
+                  selected={birthDate}
+                  onChange={(date) => {
+                    setBirthDate(date);
+                  }}
+                  dateFormat="dd/MM/yyyy"
+                  className="border border-gray-300 rounded-md w-full p-2  text-black font-light"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="recMedNumber" className="block font-medium">
+                  Nomor Rec.Med:
+                </label>
+                <input
+                  type="text"
+                  id="recMedNumber"
+                  value={recMedNumber}
+                  onChange={(e) => setRecMedNumber(e.target.value)}
+                  className="border border-gray-300 rounded-md w-full p-2 text-black font-light"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block font-medium">Jenis Kelamin:</label>
+                <div>
+                  <label className="mr-2">
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="Laki-laki"
+                      checked={gender === 'Laki-laki'}
+                      onChange={() => setGender('Laki-laki')}
+                      className="mr-1"
+                    />
+                    Laki-laki
+                  </label>
+                  <label>
+                    <input
+                      type="radio"
+                      name="gender"
+                      value="Perempuan"
+                      checked={gender === 'Perempuan'}
+                      onChange={() => setGender('Perempuan')}
+                      className="mr-1"
+                    />
+                    Perempuan
+                  </label>
+                </div>
+              </div>
+              <div className="mb-4">
+                <label htmlFor="recMedNumber" className="block font-medium">
+                  Jumlah label
+                </label>
+                <input
+                  type="number"
+                  id="number"
+                  value={num}
+                  onChange={(e) => setNum(e.target.value)}
+                  className="border border-gray-300 rounded-md w-full p-2 text-black font-light"
+                />
+              </div>
+            </form>
+            <button
+              onClick={() => {
+                const day = birthDate.getDate();
+                const year = birthDate.getFullYear(); // Mengambil tahun
+                const month = birthDate.getMonth() + 1;
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                const dateValid = day + '-' + month + '-' + year;
+                setNameArr((nameArr) => [...nameArr, name]);
+                setBirthDateArr((birthDateArr) => [...birthDateArr, dateValid]);
+                setRecMedNumberArr((recMedNumberArr) => [
+                  ...recMedNumberArr,
+                  recMedNumber,
+                ]);
+                setGenderArr((genderArr) => [...genderArr, gender]);
+                setNumArr((numArr) => [...numArr, num]);
+              }}
+              className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 mr-3"
+            >
+              Tambah
+            </button>
+            <button
+              onClick={() => handleFormSubmit()}
+              className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700"
+            >
+              Simpan
+            </button>
+          </div>
+          <div className="text-center m-8 text-2xl">Daftar Data Label</div>
+          <div className="flex justify-center">
+            {nameArr.map((value, index) => {
+              return (
+                <div className="mycard" key={index}>
+                  <table border="0">
+                    {value !== '' ? (
+                      <tr>
+                        <td>Nama</td>
+                        <td>:</td>
+                        <td>{value}</td>
+                      </tr>
+                    ) : (
+                      ''
+                    )}
+                    {birthDateArr[index] ? (
+                      <tr>
+                        <td>Tanggal Lahir</td>
+                        <td>:</td>
+                        <td>{birthDateArr[index]}</td>
+                      </tr>
+                    ) : (
+                      ''
+                    )}
+                    {recMedNumberArr[index] ? (
+                      <tr>
+                        <td>No Rec.Med</td>
+                        <td>:</td>
+                        <td>{recMedNumberArr[index]}</td>
+                      </tr>
+                    ) : (
+                      ''
+                    )}
+                    {genderArr[index] ? (
+                      <tr>
+                        <td>Jenis Kelamin</td>
+                        <td>:</td>
+                        <td>{genderArr[index]}</td>
+                      </tr>
+                    ) : (
+                      ''
+                    )}
+                    {numArr[index] ? (
+                      <tr>
+                        <td>Jumlah Copy</td>
+                        <td>:</td>
+                        <td>{numArr[index]} copy</td>
+                      </tr>
+                    ) : (
+                      ''
+                    )}
+                  </table>
+                  <div className="text-right">
+                    <button
+                      onClick={() => deleteData(index)}
+                      className="bg-red-500 text-white py-1 px-1 rounded-md hover:bg-red-700"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
+      {isResultPage && <div className="flex flex-wrap">{divElements}</div>}
+    </>
+  );
 }
